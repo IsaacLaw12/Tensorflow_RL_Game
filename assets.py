Fix image rotation and game restarting bugs

assets.py
@@ -45,7 +45,8 @@ class GameMap:
         for i in range(self.map_shape[0]):
             for j in range(self.map_shape[1]):
                 if self.map[j, i] == PLAYER_CHAR:
-                    self.player = Player(self, i*self.map_scale, j*self.map_scale)
+                    self.player = Player(self, (i*self.map_scale, j*self.map_scale))
+                    break
 
     def load_sheep(self):
         sheep_locs = []
@@ -307,6 +308,8 @@ class Player:
         global PLAYER_SPEED, PLAYER_ACCELERATION, PLAYER_SIZE, SPEED_CONVERSION
         self.map = map
         self.position = position
+        print("POSITION")
+        print(position)
         self.speed = PLAYER_SPEED * SPEED_CONVERSION
         self.manually_controlled = manually_controlled
         self.size = PLAYER_SIZE
@@ -325,7 +328,7 @@ class Player:
 
         dir = dir / max(np.linalg.norm(dir), .0001)
         proposed_position = self.position + self.speed * dir
-        if np.dot((1,1), dir):
+        if np.dot((1, 2), dir):
             self.direction = dir
         self.position = self.map.allowed_position(self.position, proposed_position, self.size)
 

run_game.py
@@ -25,6 +25,7 @@ def run_game():
     running = True
 
     while running:
+        gamemap.load_map()
         start_time = time.time()
         quit = run_game_instance(screen)
         end_time = time.time()
@@ -120,9 +121,8 @@ def rotated_image(surface_obj, direction):
     direction: unit length vector
     '''
     angle = math.acos(np.dot((0, 1), direction))
-    #opp_angle = math.acos(np.dot((0, -1), direction))
-    if direction[0] < 0:
-        angle += math.pi
+    if direction [0] < 0:
+        angle = angle * -1
     angle = angle * 180 / math.pi
     result = pygame.transform.rotate(surface_obj, angle)
     return result